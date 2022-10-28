aeab9890fe2dae33d44249bfcca525614a7aca23
YilinZ <yinix@LAPTOP-URA2JJIQ.localdomain>
2022-10-27 20:57:52 -0700

    Add Algorithm Class Templete

--- Class/Variable.py
+++ Class/Variable.py
@@ -6,37 +6,39 @@
 #       initType        --  The type of the variable from user              eg: bool, float, int
 #       initBounds      --  The bounds of the variable from user            eg: [[1,3]], [[1,3],[4,7]]
 #       initDepenBounds --  The dependent bounds of the variable from user  eg: [[-a, a^2], [a, a+1]]
-#       initIterator    --  The iterator parameter of this variable         eg: t, T
-#       initStepSize    --  The default step size of iterator               eg: 0.01
-#       initItBound     --  The bound of iterator parameter                 eg: [0, 100]
-#       initItFunc      --  The function of step size, default 0.01x        eg: x^2+2x
 #       discVal         --  The discrete value of variable                  eg: 1, 2, [1,3,5]
 #
 #
 #  One TypedVariable should have the following functions:
 #       transformIntoDiscrete()         --          Transform a given value into the discrete field, return the discrete value
+#       transformBack()                 --          Transform a given discrete value into tyoed field, return the typed value
+#       updatediscVal()                 --          Update discrete value by given value
+#       updatetmpVal()                  --          Update tmporary value by given value
 
 class TypedVariable:
-    def __init__(self, initVal, initType, initBounds, initDepenBounds, initIterator, initStepSize, initItBound, initItFunc):
+    def __init__(self, initVal, initType, initBounds, initDepenBounds):
         self.tmpVal = initVal
         self.initType = initType
         self.initBounds = initBounds
         self.initDepenBounds = initDepenBounds
-        self.initIterator = initIterator
-        self.initStepSize = initStepSize
-        self.initItBound = initItBound
-        self.initItFunc = initItFunc
+
 
     def transformIntoDiscrete(self):
         pass
 
     def transformBack(self):
         pass
+    
+    def updatediscVal(self, discVal):
+        self.discVal = discVal
+
+    def updatetmpVal(self, tmpVal):
+        self.tmpVal = tmpVal
 
 
 class Bool(TypedVariable):
-    def __init__(self, initVal, initType, initBounds, initDepenBounds, initIterator, initStepSize, initItBound, initItFunc):
-        super().__init__(initVal, initType, initBounds, initDepenBounds, initIterator, initStepSize, initItBound, initItFunc)
+    def __init__(self, initVal, initType, initBounds, initDepenBounds):
+        super().__init__(initVal, initType, initBounds, initDepenBounds)
 
     def transformIntoDiscrete(tmpVal):
         discrete = 0
@@ -51,15 +53,16 @@ class Bool(TypedVariable):
         return tmpVal
     
     def updatediscVal(self, discVal):
-        self.discVal = discVal
+        super().updatediscVal(discVal)
 
     def updatetmpVal(self, tmpVal):
-        self.tmpVal = tmpVal
+        super().updatetmpVal(tmpVal)
+
 
 
 class Float(TypedVariable):
-    def __init__(self, initVal, initType, initBounds, initDepenBounds, initIterator, initStepSize, initItBound, initItFunc):
-        super().__init__(initVal, initType, initBounds, initDepenBounds, initIterator, initStepSize, initItBound, initItFunc)
+    def __init__(self, initVal, initType, initBounds, initDepenBounds):
+        super().__init__(initVal, initType, initBounds, initDepenBounds)
 
     def transformIntoDiscrete(tmpVal):
         #TBD
@@ -67,12 +70,24 @@ class Float(TypedVariable):
     def transformBack(discVal):
         #TBD
 
+    def updatediscVal(self, discVal):
+        super().updatediscVal(discVal)
+
+    def updatetmpVal(self, tmpVal):
+        super().updatetmpVal(tmpVal)
+
 class Int(TypedVariable):
-    def __init__(self, initVal, initType, initBounds, initDepenBounds, initIterator, initStepSize, initItBound, initItFunc):
-        super().__init__(initVal, initType, initBounds, initDepenBounds, initIterator, initStepSize, initItBound, initItFunc)
+    def __init__(self, initVal, initType, initBounds, initDepenBounds):
+        super().__init__(initVal, initType, initBounds, initDepenBounds)
     
     def transformIntoDiscrete(tmpVal):
         return tmpVal
     
     def transformBack(discVal):
         return discVal
+    
+    def updatediscVal(self, discVal):
+        super().updatediscVal(discVal)
+
+    def updatetmpVal(self, tmpVal):
+        super().updatetmpVal(tmpVal)
