17d97f674f20672428a47cb8e2d82a7601b09cf7
YilinZ <yinix@LAPTOP-URA2JJIQ.localdomain>
2022-10-27 21:12:59 -0700

    Add comments on functions

--- src/parameter_types.py
+++ src/parameter_types.py
@@ -1,3 +1,21 @@
+# This file defines type classes.
+#
+# One Type class should have the following attributes:
+#       name                --  
+#       wrt                 --  
+#       init_value          --  The initial value of variable from user
+#       value_range         --  The bounds of the variable from user            eg: [[1,3]], [[1,3],[4,7]]
+#       upper_bound         --  The dependent bounds of the variable from user  eg: [[-a, a^2], [a, a+1]]
+#       lower_bound         --  The discrete value of variable                  eg: 1, 2, [1,3,5]
+#
+#
+#  One Type class should have the following functions:
+#       transformIntoDiscrete()         --          Transform a given value into the discrete field, return the discrete value
+#       transformBack()                 --          Transform a given discrete value into tyoed field, return the typed value
+#       updatediscVal()                 --          Update discrete value by given value
+#       updatetmpVal()                  --          Update tmporary value by given value
+
+
 class Int_Parameter:
     self.name = name
     self.wrt = wrt
@@ -19,6 +37,12 @@ class Int_Parameter:
     def items():
         return __dict__
 
+    def transformIntoDiscrete(val):
+        return val
+    
+    def transformBack(val):
+        return val
+
 class Bool_Parameter:
     def __init__(self, name, value_range, upper_bound, lower_bound,step,trans_function, init_value = None, wrt = False):
         self.name = name
@@ -29,6 +53,16 @@ class Bool_Parameter:
         self.lower_bound = lower_bound
         self.step = step
         self.trans_function = trans_function
+    
+    def transformIntoDiscrete(val):
+        if val:
+            return 1
+        return 0
+    
+    def transformBack(val):
+        if val == 1:
+            return True
+        return False
 
 class Float_Parameter:
     def __init__(self, name, value_range, upper_bound, lower_bound,step,trans_function, init_value = None, wrt = False):
@@ -40,4 +74,11 @@ class Float_Parameter:
         self.lower_bound = lower_bound
         self.step = step
         self.trans_function = trans_functio
+
+    def transformIntoDiscrete(val):
+        #TBD
+    
+    def transformBack(val):
+        #TBD
+
 parameter_types = {"Int_Parameter" : Int_Parameter, "Bool_Paramter" : Bool_Parameter, "Float_Parameter" : Float_Parameter}
