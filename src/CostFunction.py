eea5059074606ccbc61ece34d56553f086fc3d88
YilinZ <yinix@LAPTOP-URA2JJIQ.localdomain>
2022-11-09 11:58:00 -0800

    use ast tree to parse cost function

--- src/CostFunction.py
+++ src/CostFunction.py
@@ -1,6 +1,9 @@
+import ast
+
 class CostFunction:
     def __init__(self, formula):
         self.formula = formula
+        self.astree = ast.parse(cost_function.formula)
     
     def evaluate(self, parameter_list):
         #TBD
